4c1c9b7d4fc97aba85728cba5053d45c825a7bc9
V School Student <student@Vs-Mac-Pro-6.local>
2017-07-18 16:06:04 -0600

    first commit

---
+++ exercises/grocery-shopper/app.js
@@ -0,0 +1,18 @@
+var shopper = {
+    name: 'Tom',
+    cost: 9,
+    isMale: true,
+    isOld: function(){
+        console.log('I am not old')
+    },
+    groceryCart: ['apples', 'pears', 'lettuce']
+};
+
+console.log(shopper.isOld())
+    
+//object has key:value
+
+//string anything in quotes ''
+//number
+//boolean true or false
+//function
---
+++ warmups/Hello-Class/app.js
@@ -0,0 +1,6 @@
+function hello () {
+    return 'hello world';
+    
+}
+
+hello()
