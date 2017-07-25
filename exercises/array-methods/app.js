1371949cb33f4881cdd20afaa5fe7728a99e8c84
jmjarman <jmjarman@gmail.com>
2017-07-25 13:37:17 -0600

    array methods

---
+++ exercises/array-methods/app.js
@@ -0,0 +1,19 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];  
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"]; 
+
+vegetables.pop();
+console.log("vegetables", vegetable);
+ 
+var b = fruit.slice(0,1);
+console.log("fruit", fruit);
+
+var c = fruit.indexOf("orange");
+console.log("fruit", fruit);
+
+fruit.push("c");
+console.log("fruit", fruit);
+
+var food = fruit.concat(vegetables);
+console.log("food", food);
+
+
---
+++ warmups/no-spoon/app.js
@@ -0,0 +1,12 @@
+var person = {
+  name:  "Sarah",
+  address:
+      {
+    street: "123 st",
+    city: "slc",
+    zip:  "90210"
+  }
+ 
+}
+
+console.log(person.address.city);
